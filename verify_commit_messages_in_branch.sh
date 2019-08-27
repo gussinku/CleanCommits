9cad97d5b837311b8df4e7d0621b9d86780c0b77
Kundananji Sinkala <kundananji@Kundananjis-MacBook.local>
2019-08-27 14:29:40 +0200

    pyscript commit message blocker

---
+++ verify_commit_messages_in_branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A helper script to verify the messages of every commit in
+# the current branch when compared against `origin/master`.
+# If there is no difference then the latest commit will be used.
+set -eu
+
+script_dir=$(dirname "$0")
+commits_since_master=$(git rev-list HEAD ^origin/master)
+
+while read -r commit_hash; do
+    commit_message="$(git log --format=%B -n 1 $commit_hash)"
+    python3 $script_dir/bad_commit_message_blocker.py --message "$commit_message"
+done <<< "$commits_since_master"
